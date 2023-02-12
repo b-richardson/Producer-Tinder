b012d899ef000c98f69fc71b19363f0d79d18156
b-richardson <97001156+b-richardson@users.noreply.github.com>
2023-02-12 12:51:35 -0500

    React Frontend and Express Backend are connected!

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const expressReactViews = require('express-react-views');
+const path = require('path');
+const app = express();
+// const routes = require("./routes/index");
+const port = process.env.PORT || 5000;
+const dbConnection = process.env.DB_CONNECTION;
+const connectDB = require('./config/database');
+
+require('dotenv').config({
+    path: path.resolve(__dirname, 'config/.env'),
+  });
+
+//   app.get('/', function (req, res){
+//     res.render('homepage');
+//   });    
+  
+// app.get('/', function (req, res){
+//   res.send('hey guys');
+// });  
+
+connectDB();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jsx');
+app.engine('jsx', expressReactViews.createEngine());
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/express_backend', (req, res) => { //Line 9
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+  });
+
+
+// app.use("/", routes);
