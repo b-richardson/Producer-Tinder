edc971ab173c5a1fa3c35ec3ed8d642f72fcd6cc
b-richardson <97001156+b-richardson@users.noreply.github.com>
2023-02-13 20:35:37 -0500

    quick clean up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 import{ BrowserRouter, Routes, Route } from 'react-router-dom';
 // import logo from './logo.svg';
@@ -52,6 +26,7 @@ state = {
     return body;
   };
 
+      // This renders the pages through the browser-router. routes for all pages go below. 
   render() {
     return (
       <BrowserRouter>
@@ -62,17 +37,5 @@ state = {
     );
   }
 }
-
-
-
-// <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">{this.state.data}</p>
-      // </div>
-
-
 
 export default App;
